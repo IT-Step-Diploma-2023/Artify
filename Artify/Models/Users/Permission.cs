8ecb1470489f9893c882b66aeb9a6fb92c38db30
MichaelFadyeyev <mihail.fadeev@ukr.net>
2023-08-01 21:02:14 +0300

    Add Permission model

---
+++ Artify/Models/Users/Permission.cs
@@ -0,0 +1,11 @@
+﻿namespace Artify.Models.Users
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+        public string Tag { get; set; } = string.Empty;
+
+        // NAVIGATION PROPERTIES
+        public virtual List<UserRole> Roles { get; } = new();
+    }
+}
