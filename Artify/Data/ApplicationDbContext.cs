e3f78ca2c89ef42b4eabb745da8311fe70db9f4e
JIeHka <pogorelova_elena_1989@ukr.net>
2023-08-02 14:26:45 +0300

    Корегування класів моделей

--- Artify/Data/ApplicationDbContext.cs
+++ Artify/Data/ApplicationDbContext.cs
@@ -2,6 +2,9 @@
 using Artify.Models.Users;
 using System.Security.Claims;
 using Artify.Models.Artworks;
+using Artify.Models.Artworks.Attributes;
+using Artify.Models.Users.Attributes;
+using Artify.Models.WorkPreferenceModels;
 
 namespace Artify.Data
 {
@@ -12,6 +15,38 @@ namespace Artify.Data
         { }
         public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<User> Users { get; set; }
+        //
+
+        public DbSet<Appreciation> Appreciations { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Tool> Tools { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Shot> Shots { get; set; }
+        
+        public DbSet<Billing> Billings { get; set; }
+        public DbSet<EmployerComment> EmployerComments { get; set; }
+        public DbSet<SocialProfile> SocialProfiles { get; set; }
+        public DbSet<UserFollower> UserFollowers { get; set; }
+        public DbSet<UserSocialProfile> UserSocialProfiles { get; set; }
+        public DbSet<Employer> Employers { get; set; }
+        public DbSet<Follower> Followers { get; set; }
+        public DbSet<JwtUser> JwtUsers { get; set; } //????
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<UserLogin> UserLogins { get; set; }
+
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<FreelanceAvailability> FreelanceAvailabilities { get; set; }
+        public DbSet<FullTimeAvailability> FullTimeAvailabilities { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Salary> Salaries { get; set; }
+        public DbSet<Speciality> Specialities { get; set; }
+        public DbSet<Vacancy> Vacancies { get; set; }
+        public DbSet<WorkPreference> WorkPreferences { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
