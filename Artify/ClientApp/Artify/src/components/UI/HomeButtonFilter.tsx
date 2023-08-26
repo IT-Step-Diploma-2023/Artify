d6c264c42ea5983c75c80e5f5290f1987d9a8b91
MichaelFadyeyev <mihail.fadeev@ukr.net>
2023-08-26 14:57:02 +0300

    add styles for serch button

--- Artify/ClientApp/Artify/src/components/UI/HomeButtonFilter.tsx
+++ Artify/ClientApp/Artify/src/components/UI/HomeButtonFilter.tsx
@@ -3,7 +3,7 @@ import FilterList from '@mui/icons-material/FilterList';
 import { useTranslation } from 'react-i18next';
 import { colors } from '../../assets/defaults/colors'
 import { styled } from '@mui/system';
-
+import { Typography } from '@mui/material';
 //
 
 
@@ -12,14 +12,14 @@ export default function HomeButtonFilter() {
 
 
   return (<>
-    <Button className='button button-light button-m'
+    <Button className='button button-border-dark button-m'
       style={{
         display: 'inline',
-        border: `1px solid${colors.darkViolet}`,
         width: '200px',
-        color: `${colors.darkViolet}`,
       }}>
-      {t('фільтр')}
+      <Typography component='div' sx={{}}>
+        {t('фільтр')}
+      </Typography>
       <FilterList style={{ width: '20px', height: '20px' }} />
     </Button>
   </>
