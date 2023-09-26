2e21ba96b3ace792c8f00d27ae8011bfec760fb9
Vladyslav Shvets <vladislavshvetsua@gmail.com>
2023-09-26 10:24:49 +0200

    Added UpdateUserSocialProfiles endpoint

--- Artify/Controllers/users/UsersApiController.cs
+++ Artify/Controllers/users/UsersApiController.cs
@@ -7,15 +7,16 @@ using Artify.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json.Serialization;
-
 namespace Artify.Controllers.users
 {
     [ApiController]
     public class UsersApiController : ControllerBase
     {
         private UsersRepository _usersRepository;
-        public UsersApiController(IRepository<User> usersRepository) {
+        private SocialProfilesRepository _userProfilesRepository;
+        public UsersApiController(IRepository<User> usersRepository, IRepository<SocialProfile> userProfilesRepository) {
             this._usersRepository = (UsersRepository)usersRepository;
+            this._userProfilesRepository = (SocialProfilesRepository)userProfilesRepository;
         }
 
         /// <summary>
@@ -24,7 +25,7 @@ namespace Artify.Controllers.users
         /// /// <response code="200">Returns user in json format</response>
         /// <response code="404">UserDTO was not found in the database</response>
         /// <response code="500">Can't fetch user right now</response>
-        [Route("api/[controller]/[action]")]
+        [Route("api/users/[controller]/[action]")]
         [HttpGet]
         [Authorize]
         public IActionResult GetCurrentUserData()
@@ -54,7 +55,7 @@ namespace Artify.Controllers.users
         /// /// <response code="200">Returns user with the social profiles in json format</response>
         /// <response code="404">UserDTO was not found in the database</response>
         /// <response code="500">Can't fetch user right now</response>
-        [Route("api/[controller]/[action]")]
+        [Route("api/users/[controller]/[action]")]
         [HttpGet]
         [Authorize]
         public IActionResult GetUserSocialProfiles()
@@ -87,5 +88,74 @@ namespace Artify.Controllers.users
                 return BadRequest(new { errorMessage = "Something went wrong" });
             }
         }
+
+        [Route("api/users/[controller]/[action]")]
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> UpdateUserSocialProfiles(ListSocialProfileDTO socialProfiles)
+        {
+            try
+            {
+                JwtUser? model = UsersService.GetCurrentUser(this.HttpContext);
+                if (model == null)
+                    return Forbid();
+                User? user = _usersRepository.Query(user => user.Id == model.Id).FirstOrDefault();
+                if (user == null) return NotFound(new { errorMessage = "UserDTO was not found in the database" });
+
+                var userProfiles = _userProfilesRepository.Query(profile => profile.UserSocialProfiles.Where(item => item.UserId == user.Id).Count() > 0).AsQueryable();
+
+                foreach (SocialProfileDTO profile in socialProfiles.SocialProfiles)
+                {
+                    var existingProfile = userProfiles.Where(uprofile => uprofile.Name.ToLower() == profile.Name.ToLower()).FirstOrDefault();
+                    if (existingProfile != null)
+                    {
+                        existingProfile.UserSocialProfiles.RemoveAll(item => item.UserId == user.Id);
+                        if (profile.Address != "")
+                        {
+                            existingProfile.UserSocialProfiles.Add(new UserSocialProfile()
+                            {
+                                Address = profile.Address,
+                                UserId = user.Id
+                            });
+                        }
+                    }
+                    else
+                    {
+                        if (profile.Address != "")
+                        {
+                            SocialProfile socialProfile = new Artify.Models.DbModels.Users.Attributes.SocialProfile()
+                            {
+                                Name = FirstCharToUpper(profile.Name.ToLower()),
+                                UserSocialProfiles = new List<UserSocialProfile>()
+                            };
+                            socialProfile.UserSocialProfiles.Add(
+                                new UserSocialProfile()
+                                {
+                                    Address = profile.Address,
+                                    UserId = user.Id
+                                });
+                            _userProfilesRepository.Add(socialProfile);
+                        }
+                    }
+                }
+                await _userProfilesRepository.SaveAsync();
+                return GetUserSocialProfiles();
+            }catch (Exception)
+            {
+                return BadRequest(new { errorMessage = "Something went wrong" });
+            }
+        }
+        public class ListSocialProfileDTO : ISocialProfilesList 
+        {
+            public List<SocialProfileDTO> SocialProfiles { get; set; } = new List<SocialProfileDTO>();
+        }
+
+        public static string FirstCharToUpper(string input) =>
+           input switch
+           {
+               null => throw new ArgumentNullException(nameof(input)),
+               "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+               _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
+           };
     }
 }
